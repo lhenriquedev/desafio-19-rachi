f65724c494f46f913697c6b99c3e47749db43072
lhenrique.dev <lhenrique.dev@gmail.com>
2022-01-20 20:06:24 -0300

    adicionando algumas animações

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,11 @@ const btnNavEl = document.querySelector(".btn-mobile-nav");
 btnNavEl.addEventListener("click", () => {
   headerEl.classList.toggle("nav-open");
 });
+
+/* ############################## SCROL REVEAL ############################## */
+const scrollReveal = ScrollReveal({
+  origin: "left",
+  distance: "30px",
+  duration: 700,
+  reset: true,
+});
